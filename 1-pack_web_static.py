89faa6b98c6b51dd33c34cdcf4a469b246e28bf3
Stephen Obialor <stevobialor@gmail.com>
2023-01-21 20:00:47 +0000

    done

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,23 +3,19 @@
     create a tar.zip file
     from web_static
 '''
-
+import os.path
 from datetime import datetime
-import os
-from fabric.api import local, task
+from fabric.api import local
 
 
-@task
 def do_pack():
     '''creates .tgz file'''
 
-    if not os.path.exists('versions'):
-        os.mkdirs('versions')
     date = datetime.now().strftime('%Y%m%d%H%M%S')
     name = f"web_static_{date}.tgz"
-    local(f"tar -zcvf versions/{name} web_static")
-
-    if os.path.exists(f"versions/{name}"):
-        return f"versions/{name}"
-    else:
+    if os.path.isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
+            return None
+    if local(f"tar -zcvf versions/{name} web_static").Failed is True:
         return None
+    return name
