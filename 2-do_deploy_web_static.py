971800c9a715091f7b79e3814a871fa452da1c39
Stephen Obialor <stevobialor@gmail.com>
2023-01-22 12:48:54 +0000

    done

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+'''
+    contains the module for setting up remote module
+'''
+
+from fabric.api import *
+import os
+
+env.hosts = ['54.165.158.100', '54.209.231.85']
+
+
+def do_deploy(archive_path):
+    '''method to set up remote servers'''
+
+    if not os.path.isfile(archive_path):
+        return False
+
+    filename = os.path.basename(archive_path)
+    release_folder = '/data/web_static/releases/' + filename.split('.')[0]
+
+    put(archive_path, '/tmp/')
+    run('mkdir -p ' + release_folder)
+    run('tar -xzf /tmp/' + filename + ' -C ' + release_folder)
+    run('rm /tmp/' + filename)
+    run('rm -rf /data/web_static/current')
+    run('ln -s ' + release_folder + ' /data/web_static/current')
+
+    return True
