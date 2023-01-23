341ea0e3342d18369bcc8acae42f2bcbcd3dc1c1
Stephen Obialor <stevobialor@gmail.com>
2023-01-23 05:13:00 +0000

    done

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,22 +3,23 @@
     contains the module for setting up remote module
 '''
 
-from fabric.api import *
+from fabric.api import put, run
 import os
 
 env.hosts = ['ubuntu@54.165.158.100', 'ubuntu@54.209.231.85']
 
 
-def do_deploy(archive_path):
+def do_deploy():
     '''method to set up remote servers'''
 
-    if not os.path.isfile(archive_path):
+    a_path = '~/AirBnB_clone_v2/versions/web_static_20230122121617.tgz'
+    if not os.path.exists(a_path):
         return False
 
-    filename = os.path.basename(archive_path)
-    release_folder = '/data/web_static/releases/' + filename.split('.')[0]
+    filename = 'web_static_20230122121617.tgz'
+    release_folder = '/data/web_static/releases/web_static_20230122121617'
 
-    put(archive_path, '/tmp/')
+    put(a_path, '/tmp/')
     run('mkdir -p ' + release_folder)
     run('tar -xzf /tmp/' + filename + ' -C ' + release_folder)
     run('rm /tmp/' + filename)
