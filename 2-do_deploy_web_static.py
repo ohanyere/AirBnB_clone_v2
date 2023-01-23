0ab49fdb10d0fc3534400061758cd4d9e35bb38a
Stephen Obialor <stevobialor@gmail.com>
2023-01-23 18:29:04 +0000

    done

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,29 +1,36 @@
 #!/usr/bin/python3
-'''
-    contains the module for setting up remote module
-'''
+""" a Fabric script that distributes an
+    archive to your web servers, using the function do_deploy
+"""
+
 
 from fabric.api import *
-import os
+from datetime import datetime
+from os.path import exists
+
 
 env.hosts = ['54.165.158.100', '54.209.231.85']
 
 
-def do_deploy():
-    '''method to set up remote servers'''
-
-    a_path = '~/AirBnB_clone_v2/versions/web_static_20230122121617.tgz'
-    if not os.path.exists(a_path):
+def do_deploy(archive_path):
+    """ distributes an archive to my web servers
+    """
+    if exists(archive_path) is False:
         return False
+    filename = archive_path.split('/')[-1]
+    a_tgz = '/data/web_static/releases/' + "{}".format(filename.split('.')[0])
+    tmp = "/tmp/" + filename
 
-    filename = 'web_static_20230122121617.tgz'
-    release_folder = '/data/web_static/releases/web_static_20230122121617'
+    try:
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}/".format(a_tgz))
+        run("tar -xzf {} -C {}/".format(tmp, a_tgz))
+        run("rm {}".format(tmp))
+        run("mv {}/web_static/* {}/".format(a_tgz, a_tgz))
+        run("rm -rf {}/web_static".format(a_tgz))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {}/ /data/web_static/current".format(a_tgz))
 
-    put(a_path, '/tmp/')
-    run('mkdir -p ' + release_folder)
-    run('tar -xzf /tmp/' + filename + ' -C ' + release_folder)
-    run('rm /tmp/' + filename)
-    run('rm -rf /data/web_static/current')
-    run('ln -s ' + release_folder + ' /data/web_static/current')
-
-    return True
+        return True
+    except:
+        return False
