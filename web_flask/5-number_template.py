69babf65987628f59158c64f607b2c9cb54966cb
Stephen Obialor <stevobialor@gmail.com>
2023-02-04 14:45:06 +0000

    done

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+'''
+    contains the hello HBNB functions
+'''
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    '''prints hello HBNB to screen'''
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    '''displays just HBNB'''
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def user_text(text):
+    '''displays c with a text'''
+    return 'C {}'. format(text.replace('_', ' '))
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def pyth_txt(text="is cool"):
+    '''displays python with a text'''
+    return 'Python {}'. format(text.replace('_', ' '))
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    '''returns a number text'''
+    return "{} is a number". format(n)
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number(n):
+        '''returns a number text'''
+        return render_template('templates/5-number.html', n=n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
