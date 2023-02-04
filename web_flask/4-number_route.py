2fef9c39a4f0a23e976baae3293c9ef443cc6bd5
Stephen Obialor <stevobialor@gmail.com>
2023-02-04 14:28:48 +0000

    done

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+'''
+    contains the hello HBNB functions
+'''
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    '''prints hello HBNB to screen'''
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    '''displays just HBNB'''
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def user_text(text):
+    '''displays c with a text'''
+    return 'C {}'. format(text.replace('_', ' '))
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def pyth_txt(text="is cool"):
+    '''displays python with a text'''
+    return 'Python {}'. format(text.replace('_', ' '))
+
+
+@app.route("/number/<n>", strict_slashes=False)
+def number(n):
+    if (type(n) == int):
+        return "{} is a number". format(n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
