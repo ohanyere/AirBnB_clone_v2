21a6377867d41f86e6c89e902542c83111375e9c
Stephen Obialor <stevobialor@gmail.com>
2023-02-04 10:47:06 +0000

    done

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+'''
+    contains the hello HBNB functions
+'''
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    '''prints hello HBNB to screen'''
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    '''displays just HBNB'''
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def user_text(text):
+    return 'C {}'. format(text.replace('_', ' '))
+
+
+@app.route("/python/(<text>)", strict_slashes=False)
+def pyth(text="is cool"):
+    return 'Python {}'. format(text.replace('_', ' '))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
