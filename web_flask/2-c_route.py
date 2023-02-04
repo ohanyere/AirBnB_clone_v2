515a48bbbed181fa29d457bcc7d003dd244ff405
Stephen Obialor <stevobialor@gmail.com>
2023-02-04 10:43:32 +0000

    done

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -22,7 +22,7 @@ def hbnb():
 
 @app.route("/c/<text>", strict_slashes=False)
 def user_text(text):
-    return 'C{}'. format(text.replace('_', ' '))
+    return 'C {}'. format(text.replace('_', ' '))
 
 
 if __name__ == "__main__":
