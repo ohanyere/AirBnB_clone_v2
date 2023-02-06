42b0ae62bc54f7f5603d5c5becc419b46d0e1b9b
ohanyere <mooref068@gmail.com>
2023-02-06 04:09:44 -0800

    a script that starts a Flask web application

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,42 +1,36 @@
 #!/usr/bin/python3
 '''
-    module to start a flask app
+Script that starts a Flask web appliction and lists all the states and
+cities objects alphabeticallly in A->Z. This also includes the HTML pages
 '''
-
 from flask import Flask, render_template
 from models import storage
+from models.state import State
 
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def remove_session(error):
-    '''
-        tears down the session
-    '''
+def close_database(self):
+    '''Closes all sqlalchemy session in the database'''
     storage.close()
 
 
-@app.route("/states", strict_slashes=False)
-def states_list():
-    '''returns a template of unordered lists of state
-        objects
-    '''
-    return render_template("9-states.html", states=storage.all('State'))
+@app.route('/states/', strict_slashes=False)
+def states():
+    '''Returns the list of all states'''
+    states = storage.all(State)
+    return render_template('9-states.html', states=states, mode='all')
 
 
 @app.route('/states/<id>', strict_slashes=False)
-def state_id(id):
-    states = storage.all('State')
-    try:
-        state_id = states[id]
-        return render_template(
-            '9-states.html',
-            state_id=state_id,
-            condition="state_id")
-    except ValueError:
-        return render_template('9-states.html', condition="not_found")
+def states_id(id):
+    '''Returns the list all the states by id'''
+    for state in storage.all(State).values():
+        if state.id == id:
+            return render_template('9-states.html', states=state, mode='id')
+    return render_template('9-states.html', states=state, mode='none')
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000)
